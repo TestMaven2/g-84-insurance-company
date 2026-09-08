f952948da30b8bc8c219822b4a457a87d6936b52
AndreyPomelov <andrei.pomelov1@gmail.com>
2026-09-08 13:54:20 +0200

    chat history support implemented

---
+++ src/chat/prompt.builder.ts
@@ -0,0 +1,61 @@
+import { Role } from '../users/enums/role.enum';
+import { ChatMessage } from './types/chat-message';
+
+export class PromptBuilder {
+  private prompt: string;
+
+  constructor(basePrompt: string) {
+    this.prompt = basePrompt;
+  }
+
+  withUserRole(role: Role): PromptBuilder {
+    this.prompt = `${this.prompt}
+    
+Роль пользователя:
+${role}`;
+    return this;
+  }
+
+  withContext(chunks: string[]): PromptBuilder {
+    this.prompt = `${this.prompt}
+    
+Контекст:
+
+${chunks.join('\n\n')}
+
+Конец контекста.`;
+    return this;
+  }
+
+  withChatHistory(chatHistory: ChatMessage[]): PromptBuilder {
+    this.prompt = `${this.prompt}
+    
+История диалога:
+
+${this.mapChatHistoryToMultistring(chatHistory)}
+
+Конец истории диалога.`;
+    return this;
+  }
+
+  private mapChatHistoryToMultistring(chatHistory: ChatMessage[]): string {
+    return chatHistory
+      .map(
+        (m: ChatMessage): string =>
+          `Вопрос пользователя: ${m.userRequest}\nОтвет ИИ ассистента: ${m.aiAnswer}`,
+      )
+      .join('\n\n');
+  }
+
+  withQuestion(request: string): PromptBuilder {
+    this.prompt = `${this.prompt}
+    
+Вопрос пользователя:
+${request}`;
+    return this;
+  }
+
+  build(): string {
+    return this.prompt;
+  }
+}
